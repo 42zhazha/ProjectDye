1bbcdb01f13a23d8f33c3e4386348962618310f0
42zhazha <42zhazha@gmail.com>
2019-05-27 01:08:35 +0800

    up

--- Assets/Scripts/UI/ScoreUI.cs
+++ Assets/Scripts/UI/ScoreUI.cs
@@ -7,8 +7,10 @@ using DG.Tweening;
 public class ScoreUI : Windows
 {
     int countdown = 30;
+    [SerializeField] Sprite[] scoreLevelSprites;
+    [SerializeField] Sprite[] scoreOSprites;
 
-    [SerializeField] Image scoreLevel;
+    [SerializeField] Image scoreLevel, scoreO;
     [SerializeField] Text timeText, countText, replayText;
     [SerializeField] Transform iconPoint;
 
@@ -17,19 +19,131 @@ public class ScoreUI : Windows
         gameObject.SetActive(true);
         timeText.text = Mathf.FloorToInt(time / 60f).ToString() + ":" + (time % 60).ToString("00");
         countText.text = cloths.Length.ToString();
+        InvokeRepeating("OnCountDown", 1, 1);
         for (int i = 0; i < cloths.Length; i++)
         {
             GameObject obj = new GameObject("", typeof(Image), typeof(Outline));
             obj.GetComponent<Image>().sprite = Resources.Load<Sprite>("Logo/Recipe/" + cloths[i]);
             obj.transform.SetParent(iconPoint);
         }
-        InvokeRepeating("OnCountDown", 1, 1);
+        float offset = time - (new float[5] { 130f, 200f, 230f, 260f, 290f }[GameManager.Instance.level - 1]);
+        print(offset);
+        if (offset < 5)
+        {
+            //A+
+            scoreLevel.sprite = scoreLevelSprites[0];
+            scoreO.sprite = scoreOSprites[0];
+        }
+        else if (offset < 10)
+        {
+            //A
+            scoreLevel.sprite = scoreLevelSprites[0];
+            scoreO.sprite = scoreOSprites[1];
+        }
+        else if (offset < 20)
+        {
+            //A-
+            scoreLevel.sprite = scoreLevelSprites[0];
+            scoreO.sprite = scoreOSprites[2];
+        }
+        else if (offset < 30)
+        {
+            //B+
+            scoreLevel.sprite = scoreLevelSprites[1];
+            scoreO.sprite = scoreOSprites[0];
+        }
+        else if (offset < 40)
+        {
+            //B
+            scoreLevel.sprite = scoreLevelSprites[1];
+            scoreO.sprite = scoreOSprites[1];
+        }
+        else if (offset < 50)
+        {
+            //B-
+            scoreLevel.sprite = scoreLevelSprites[1];
+            scoreO.sprite = scoreOSprites[2];
+        }
+        else if (offset < 60)
+        {
+            //c+
+            scoreLevel.sprite = scoreLevelSprites[2];
+            scoreO.sprite = scoreOSprites[0];
+        }
+        else if (offset < 70)
+        {
+            //c
+            scoreLevel.sprite = scoreLevelSprites[2];
+            scoreO.sprite = scoreOSprites[1];
+        }
+        else if (offset < 80)
+        {
+            //c-
+            scoreLevel.sprite = scoreLevelSprites[2];
+            scoreO.sprite = scoreOSprites[2];
+        }
+        else if (offset < 90)
+        {
+            //D+
+            scoreLevel.sprite = scoreLevelSprites[3];
+            scoreO.sprite = scoreOSprites[0];
+        }
+        else if (offset < 100)
+        {
+            //D
+            scoreLevel.sprite = scoreLevelSprites[3];
+            scoreO.sprite = scoreOSprites[1];
+        }
+        else if (offset < 110)
+        {
+            //D-
+            scoreLevel.sprite = scoreLevelSprites[3];
+            scoreO.sprite = scoreOSprites[2];
+        }
+        else if (offset < 120)
+        {
+            //E+
+            scoreLevel.sprite = scoreLevelSprites[4];
+            scoreO.sprite = scoreOSprites[0];
+        }
+        else if (offset < 130)
+        {
+            //E
+            scoreLevel.sprite = scoreLevelSprites[4];
+            scoreO.sprite = scoreOSprites[1];
+        }
+        else if (offset < 140)
+        {
+            //E-
+            scoreLevel.sprite = scoreLevelSprites[4];
+            scoreO.sprite = scoreOSprites[2];
+        }
+        else if (offset < 150)
+        {
+            //F+
+            scoreLevel.sprite = scoreLevelSprites[5];
+            scoreO.sprite = scoreOSprites[0];
+        }
+        else if (offset < 160)
+        {
+            //F
+            scoreLevel.sprite = scoreLevelSprites[5];
+            scoreO.sprite = scoreOSprites[1];
+        }
+        else
+        {
+            //F-
+            scoreLevel.sprite = scoreLevelSprites[5];
+            scoreO.sprite = scoreOSprites[2];
+        }
+
+
     }
 
     void OnCountDown()
     {
         countdown -= 1;
-        if (countdown == 0)
+        if (countdown <= 0)
             SceneManager.LoadScene("Init");
         else
             replayText.text = "按下2號鍵繼續(" + countdown.ToString() + ")";
