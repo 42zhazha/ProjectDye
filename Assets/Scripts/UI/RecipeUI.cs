5421468ae571aefb5b5d7932281c3c3e51448925
42zhazha <42zhazha@gmail.com>
2019-06-05 13:59:12 +0800

    up

--- Assets/Scripts/UI/RecipeUI.cs
+++ Assets/Scripts/UI/RecipeUI.cs
@@ -5,12 +5,19 @@ using UnityEngine.UI;
 public class RecipeUI : MonoBehaviour
 {
     [SerializeField] Image recipeImage, headImage;
-
+    [SerializeField] Transform resourcesPoint;
     public CuisineData cuisine { get; private set; }
     public void SetCuisineData(CuisineData data)
     {
         this.cuisine = data;
         headImage.sprite = Resources.Load<Sprite>("Logo/Recipe/Color/" + this.cuisine.name);
         recipeImage.sprite = Resources.Load<Sprite>("Logo/Recipe/" + this.cuisine.name);
+        for (int i = 0; i < this.cuisine.formula.Length; i++)
+        {
+            GameObject obj = new GameObject("Resources", typeof(Image), typeof(Outline));
+            obj.transform.SetParent(resourcesPoint);
+            obj.transform.localScale = Vector3.one;
+            obj.GetComponent<Image>().sprite = Resources.Load<Sprite>("Logo/Resources/" + this.cuisine.formula[i]);
+        }
     }
 }
